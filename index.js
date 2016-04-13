404912e917579b38588f2813592879fd46623144
Olivier Lesnicki <olivier.lesnicki@gmail.com>
2016-04-13 10:55:40 +0100

    Initial commit

--- index.js
+++ index.js
@@ -2,5 +2,8 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-issue-app-import-addon'
+  name: 'ember-cli-issue-app-import-addon',
+  included(app) {
+    app.import('vendor/styles/background-black.css');
+  },
 };
